0062622dd44810d377f0f3e04a3313b4574f30f4
Margo <margo@margonline.co.uk>
2013-09-12 18:20:02 +0100

    recursive solution

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,10 +1,15 @@
 class Palindrome
 
-  def compute(n)
-    n_array = n.to_s.chars
-    n_array.first == n_array.last
+  
+
+  def compare?(array)
+    return true if array.length <= 1
+    array[0] == array[-1] && compare?(array[1..-2])
   end
 
-
-
+  def num_to_a(n)
+    digits = n.to_s.chars
+    compare?(digits) 
+  end
 end
+
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -4,16 +4,19 @@ describe Palindrome do
   let(:palindrome) {Palindrome.new}
   # base case
   it "returns true if the number is one digit in length" do
-    expect(palindrome.compute(2)).to be true
+    expect(palindrome.num_to_a(3)).to be_true
   end
 
   it "returns true if the digits in a 2 digit number are equal" do
-    expect(palindrome.compute(22)).to be true
-
+    expect(palindrome.num_to_a(22)).to be_true
   end
 
-  it "returns true if the digits in a 2 digit number are equal" do
-      expect(palindrome.compute(21)).to be false
+  it "returns false if the digits in a 2 digit number are not equal" do
+    expect(palindrome.num_to_a(21)).to be_false
+  end
+
+  it "returns true if the digits in a num are the same in reverse" do
+    expect(palindrome.num_to_a(12321)).to be_true
   end
  
 
